1afd734aaada0801fd996feedd4d5ae79fa5690e
GilbertodelaCruz <gild9622@gmail.com>
2020-01-08 19:21:02 -0600

    cambios

--- WebLandingTemplate/Models/SupplierVM.cs
+++ WebLandingTemplate/Models/SupplierVM.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,9 +9,19 @@ namespace WebLandingTemplate.Models
 {
     public class SupplierVM
     {
+        [Key]
         public int ProveedorId { get; set; }
+
+        [DisplayName("Nombre")]
+        //[StringLength(maximumLength:20)]
+        [Required]
         public string Name { get; set; }
+
+        [DisplayName("Logo")]
         public byte[] Logo { get; set; }
+
+        [DisplayName("Descripción")]
+        [StringLength(maximumLength: 150)]
         public string Descripcion { get; set; }
     }
 }
