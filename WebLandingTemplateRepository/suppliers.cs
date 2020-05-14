0f1ad25e066ee84a79bca3c28308268e4edb4563
Kiva <irvin_lechugasss@hotmail.com>
2020-05-13 22:27:42 -0500

    Fix BD Tables
    Se cambiaron datos y campos en tablas

--- WebLandingTemplateRepository/suppliers.cs
+++ WebLandingTemplateRepository/suppliers.cs
@@ -24,6 +24,7 @@ namespace WebLandingTemplateRepository
         public string Name { get; set; }
         public byte[] Logo { get; set; }
         public string Descripcion { get; set; }
+        public string ContactPhone { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Products> Products { get; set; }
