3d2b4160891559f3d915c815e81e83b1435734a1
GilbertodelaCruz <gild9622@gmail.com>
2021-04-06 10:12:58 -0500

    PRODUCT FRONT

---
+++ WebLandingTemplate/Controllers/FrontEnd/ProductFrontController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebLandingTemplate.Models.FrontVIewModels;
+using WebLandingTemplateBusinessLogic.Interface;
+using WebLandingTemplateBusinessLogic.Logic;
+
+namespace WebLandingTemplate.Controllers.FrontEnd
+{
+    public class ProductFrontController : Controller
+    {
+        IProductBusiness _productBusiness;
+        ISupplierBusiness _supplierBusiness;
+        ICategoryBusiness _categoryBusiness;
+
+
+        public ProductFrontController(ProductBusiness productBusiness, SupplierBusiness supplierBusiness, CategoryBusiness categoryBusiness)
+        {
+            _productBusiness = productBusiness;
+            _supplierBusiness = supplierBusiness;
+            _categoryBusiness = categoryBusiness;
+        }
+        // GET: ProductFront
+        public ActionResult Index()
+            {
+            var listaDto = _categoryBusiness.GetAllCategory().ToList();
+            var prodFrontVM = new ProductFrontVM();
+           // listaDto = prodFrontVM.categoryVMs;
+            AutoMapper.Mapper.Map(listaDto, prodFrontVM.categoryVMs);
+           
+            return View(prodFrontVM);
+            }
+        
+    }
+}
---
+++ WebLandingTemplate/Models/FrontVIewModels/ProductFrontVM.cs
@@ -0,0 +1,16 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebLandingTemplate.Models.FrontVIewModels
+{
+    public class ProductFrontVM
+    {
+
+
+      public List<CategoryVM> categoryVMs { get; set; }
+    }
+
+   
+}
